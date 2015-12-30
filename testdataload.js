d077f32bb5c7af636005b897426f9a58aa70d5ea
Jim Campbell <jcampbell@dekaresearch.com>
2015-12-30 13:31:25 -0500

    added script to load mongo

---
+++ testdataload.js
@@ -0,0 +1,12 @@
+
+print("loading testdataload.js");
+db = db.getSiblingDB('CChat');
+print ("dropping CChat database");
+db.dropDatabase();
+print ("loading new CChat database");
+db = db.getSiblingDB('CChat');
+db.users.insert({ "username" : "Jim", "email" : "jim@smallcampbell.com" })
+db.users.insert({ "username" : "Lucas", "email" : "lucascmpbll@gmail.com" })
+db.users.insert({ "username" : "Betsy", "email" : "Betsy@smallcampbell.com" })
+db.users.insert({ "username" : "Lilly", "email" : "l_campbell@dublinschool.org" })
+print ("all done")
